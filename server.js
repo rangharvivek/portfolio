f87956c15ccae8b02527661e27a054d22c5823ea
vivek <[200~rangharvivek003@gmail.com>
2025-09-06 09:01:07 +0100

    added resume

--- server.js
+++ server.js
@@ -12,16 +12,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://viveksinghranghar:uBzaSPeRwkM1WdHn@cluster0.z2j6ycw.mongodb.net/portfolio";
 
-// Connect to MongoDB
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(()=> console.log("✅ MongoDB connected"))
   .catch(err => console.error("❌ MongoDB connection error:", err));
 
-// Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// static files (css/js/images)
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(session({
@@ -31,7 +28,6 @@ app.use(session({
 }));
 app.use(flash());
 
-// Make flash & session available in views
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
@@ -39,13 +35,10 @@ app.use((req, res, next) => {
   next();
 });
 
-// View engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-// Routes
 app.use("/", portfolioRoutes);
 app.use("/admin", adminRoutes);   // 👈 yeh add karo
 
-// Start
 app.listen(PORT, ()=> console.log(`🚀 Server running: http://localhost:${PORT}`));
