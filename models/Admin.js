2aafe5699f9d9d30e9a2718d1d5360a98b289041
rangharvivek <rangharvivek003@gmail.com>
2025-09-06 13:41:02 +0100

    Update Admin.js

--- models/Admin.js
+++ models/Admin.js
@@ -1,8 +1,21 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const adminSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true } // hashed
+  password: { type: String, required: true }
+});
+
+// Pre-save hook to hash password
+adminSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = mongoose.model("Admin", adminSchema);
